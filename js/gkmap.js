ef5865b5455db17734972402ce9c54f4257d17e4
Mathieu Alorent <github@kumy.net>
2015-09-18 00:38:00 +0200

    initial commit

---
+++ js/gkmap.js
@@ -0,0 +1,59 @@
+var map;
+var geoJsonLayer;
+var markers = L.markerClusterGroup({maxClusterRadius: 40});
+
+
+function initmap() {
+  // set up the map
+  map = new L.Map('map');
+
+  // create the tile layer with correct attribution
+  var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+  var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});   
+
+  map.addLayer(osm);
+  // start the map at Paris
+  map.setView(new L.LatLng(43.5943, 6.9509), 8);
+
+  //map.locate({setView: true, maxZoom: 16});
+}
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+function retrieve() {
+  var bounds = map.getBounds();
+  var url="//api.geokretymap.org/export2.php?latTL="+bounds.getNorth()+"&lonTL="+bounds.getEast()+"&latBR="+bounds.getSouth()+"&lonBR="+bounds.getWest()+"&limit=500&json";
+
+  jQuery.ajax({
+    dataType: "json",
+    url: url,
+    success: function(geojsonFeature){
+      if (geoJsonLayer != undefined) {
+            markers.removeLayer(geoJsonLayer);
+      }
+      geoJsonLayer = L.geoJson(geojsonFeature, {
+        onEachFeature: onEachFeature
+      });
+      markers.addLayer(geoJsonLayer);
+      map.addLayer(markers);
+    },
+    error: function(xhr){
+      var err = eval("(" + xhr.responseText + ")");
+      window.console.log(err.Message);
+    }
+  });
+}
+
+
+initmap();
+retrieve();
+
+map.on('moveend', function() {
+  retrieve();
+});
